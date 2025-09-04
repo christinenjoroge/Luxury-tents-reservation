9a6b1a4f89188530b8c3208f4c179efb3a8c3a3f
christinenjorge <christinenjoroge79@gmail.com>
2025-09-04 13:57:56 +0300

    made changes in the function.php

--- function.php
+++ function.php
@@ -5,9 +5,12 @@ function luxurycamp_enqueue_scripts() {
     wp_enqueue_style('luxurycamp-style', get_stylesheet_uri());
 
     // Load your custom style.css (in theme root, no folder)
-    wp_enqueue_style('custom-css', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('custom-css', get_template_directory_uri() . 'style.css');
 
     // Load your main.js (also in theme root)
-    wp_enqueue_script('luxurycamp-script', get_template_directory_uri() . '/javascript.js', array('jquery'), null, true);
+    wp_enqueue_script('luxurycamp-script', get_template_directory_uri() . 'javascript.js', array('jquery'), null, true);
 }
 add_action('wp_enqueue_scripts', 'luxurycamp_enqueue_scripts');
+
+// Bootstrap CSS (CDN)
+    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css');
